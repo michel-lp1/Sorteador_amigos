6b89040b3879581e208d3b57856b388d4e03328b
Michel Lopes <Michel.lopes@krona.net>
2023-03-02 20:32:12 -0300

    formulario

--- sorteador/src/componentes/ListaParticipantes.test.tsx
+++ sorteador/src/componentes/ListaParticipantes.test.tsx
@@ -1,40 +1,105 @@
-import { render, screen } from '@testing-library/react'
-import React from 'react'
-import { RecoilRoot } from 'recoil'
-import { useListaDeParticipantes } from '../state/hook/useListaDeParticipantes'
-import ListaParticipantes from './ListaParticipantes'
+import { act, fireEvent, render, screen } from "@testing-library/react";
+import React from "react";
+import { RecoilRoot } from "recoil";
+import Formulario from "./Formulario";
 
-jest.mock('../state/hook/useListaDeParticipantes', () => {
-    return {
-        useListaDeParticipantes: jest.fn()
-    }
-})
 
-describe('uma lista vazia de participantes', () => {
-    beforeEach(() => {
-        (useListaDeParticipantes as jest.Mock).mockReturnValue([])
+describe('o comportamento do Formulario.tsx', () => {
+    test('quando o input está vazio, novos participantes não podem ser adicionados', () => {
+        render(
+            <RecoilRoot>
+                <Formulario />
+            </RecoilRoot>)
+        // encontrar no DOM o input
+        const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+        // encontrar o botão
+        const botao = screen.getByRole('button')
+        // garantir que o input esteja no documento
+        expect(input).toBeInTheDocument()
+        // garantir que o botão esteja desabilitado
+        expect(botao).toBeDisabled()
     })
-    test('deve ser renderizada sem elementos', () => {
-        render(<RecoilRoot>
-            <ListaParticipantes />
-        </RecoilRoot>)
     
-        const itens = screen.queryAllByRole('listitem')
-        expect(itens).toHaveLength(0)
-    })
-})
-
-describe('uma lista preenchida de participantes', () => {
-    const participantes = ['Ana', 'Catarina']
-    beforeEach(() => {
-        (useListaDeParticipantes as jest.Mock).mockReturnValue(participantes)
-    })
-    test('deve ser renderizada sem elementos', () => {
-        render(<RecoilRoot>
-            <ListaParticipantes />
-        </RecoilRoot>)
+    test('adicionar um participante caso exista um nome preenchido', () => {
+        render(
+            <RecoilRoot>
+                <Formulario />
+            </RecoilRoot>)
     
-        const itens = screen.queryAllByRole('listitem')
-        expect(itens).toHaveLength(participantes.length)
+        // encontrar no DOM o input
+        const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+        // encontrar o botão
+        const botao = screen.getByRole('button')
+    
+        // inserir um valor no input
+        fireEvent.change(input, {
+            target: {
+                value: 'Ana Catarina'
+            }
+        })
+    
+        // clicar no botão de submeter
+        fireEvent.click(botao)
+    
+        // garantir que o input esteja com o foco ativo
+        expect(input).toHaveFocus()
+        // garantir que o input não tenha um valor
+        expect(input).toHaveValue("")
+    })
+    
+    test('nomes duplicados não podem ser adicionados na lista', () => {
+        render(
+            <RecoilRoot>
+                <Formulario />
+            </RecoilRoot>)
+        const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+        const botao = screen.getByRole('button')
+        fireEvent.change(input, {
+            target: {
+                value: 'Ana Catarina'
+            }
+        })
+        fireEvent.click(botao)
+        fireEvent.change(input, {
+            target: {
+                value: 'Ana Catarina'
+            }
+        })
+        fireEvent.click(botao)
+    
+        const mensagemDeErro = screen.getByRole('alert')
+    
+        expect(mensagemDeErro.textContent).toBe('Nomes duplicados não são permitidos!')
+    })
+    
+    test('a mensagem de erro deve sumir após os timers', () => {
+        jest.useFakeTimers()
+        render(
+            <RecoilRoot>
+                <Formulario />
+            </RecoilRoot>)
+        const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+        const botao = screen.getByRole('button')
+        fireEvent.change(input, {
+            target: {
+                value: 'Ana Catarina'
+            }
+        })
+        fireEvent.click(botao)
+        fireEvent.change(input, {
+            target: {
+                value: 'Ana Catarina'
+            }
+        })
+        fireEvent.click(botao)
+        let mensagemDeErro = screen.queryByRole('alert')
+        expect(mensagemDeErro).toBeInTheDocument()
+    
+        act(() => {
+            jest.runAllTimers()
+        });
+    
+        mensagemDeErro = screen.queryByRole('alert')
+        expect(mensagemDeErro).toBeNull()
     })
 })
